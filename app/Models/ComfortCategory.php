cd66d535ddd3994e277670fdda16ed9eb7434157
Jlblcuk <NemerovVV@mail.ru>
2025-11-28 00:08:54 +0500

    Описал модели. Создал фабрики и сиды

---
+++ app/Models/ComfortCategory.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ComfortCategory extends Model
+{
+    /** @use HasFactory<\Database\Factories\ComfortCategoryFactory> */
+    use HasFactory;
+}
